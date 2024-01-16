68c90fdf16ee15d8472ee341ef2ad6aaba619edf
linqi <wkw4399xs@gmail.com>
2024-01-16 15:36:48 +0800

    Feature Commit:消息队列 workqueue 模型测试

---
+++ consumer/src/main/java/com/linqi/consumer/listener/SpringRabbitListener.java
@@ -0,0 +1,34 @@
+package com.linqi.consumer.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.converter.MessageConversionException;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description
+ */
+@Slf4j
+@Component
+public class SpringRabbitListener {
+
+    @RabbitListener(queues = "simple.queue")
+    public  void listenSimpleQueueMessage(String msg){
+        log.info("接收到消息：{}",msg);
+        log.info("消息处理完成!");
+    }
+    @RabbitListener(queues = "work.queue")
+    public  void listenWorkerQueueMessage1(String msg){
+        log.info("work.queue 1接收到消息：{}",msg);
+        log.info("work.queue 1消息处理完成!");
+    }
+
+    @RabbitListener(queues = "work.queue")
+    public  void listenWorkerQueueMessage2(String msg) throws InterruptedException {
+        log.info("work.queue 2 接收到消息：{}",msg);
+        Thread.sleep(5000);
+        log.info("work.queue 2 消息处理完成!");
+    }
+}
