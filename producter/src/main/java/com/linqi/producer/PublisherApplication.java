994da213f9a79ca37f0a30624bcf993dd9c2920f
linqi <wkw4399xs@gmail.com>
2024-01-18 16:09:22 +0800

    Feature Commit:消息队列 消息转换器 Demo 完成

--- producter/src/main/java/com/linqi/producer/PublisherApplication.java
+++ producter/src/main/java/com/linqi/producer/PublisherApplication.java
@@ -1,7 +1,10 @@
 package com.linqi.producer;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Hello world!
@@ -13,5 +16,14 @@ public class PublisherApplication
     public static void main( String[] args )
     {
         SpringApplication.run(PublisherApplication.class, args);
+    }
+
+    /**
+     * rabbitmq 消息对内消息转换器配置
+     * @return 消息转换器
+     */
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
